0a4ab7e579863364f208c1bf02ee68d5a9f2718f
Thiago Reis Nascimento <trnasci@gmail.com>
2022-08-31 14:34:30 -0300

    Resolução requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,45 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artist: '',
+    searchButton: true,
+  };
+
+  onInputChange = (event) => {
+    const { target } = event;
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      const minLength = 2;
+      if (value.length >= minLength) {
+        this.setState({ searchButton: false });
+      } else {
+        this.setState({ searchButton: true });
+      }
+    });
+  };
+
   render() {
+    const { artist, searchButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            name="artist"
+            type="text"
+            data-testid="search-artist-input"
+            value={ artist }
+            onChange={ this.onInputChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ searchButton }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
